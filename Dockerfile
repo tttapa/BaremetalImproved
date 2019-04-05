7f474a99c0d9c190a84c71fd4ddc4711cc161608
Pieter Pas <pieter.pas@outlook.com>
2019-04-05 10:11:33 +0200

    Cross compile baremetal

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 FROM ubuntu:latest
 
 RUN apt-get update
-RUN apt-get install -y git gcc-8 g++-8 cmake gdb gdbserver  && \
+RUN apt-get install -y git wget gcc-8 g++-8 cmake gdb gdbserver  && \
     apt-get clean autoclean && \
     apt-get autoremove -y && \
     rm -rf /var/lib/apt/lists/*
@@ -21,6 +21,11 @@ EXPOSE 2000
 USER develop
 WORKDIR /home/develop
 RUN git clone https://github.com/google/googletest.git
+
+RUN wget https://armkeil.blob.core.windows.net/developer/Files/downloads/gnu-a/8.3-2019.03/binrel/gcc-arm-8.3-2019.03-x86_64-arm-eabi.tar.xz
+RUN tar xf gcc-arm-8.3-2019.03-x86_64-arm-eabi.tar.xz && \
+    rm gcc-arm-8.3-2019.03-x86_64-arm-eabi.tar.xz
+ENV PATH="/home/develop/gcc-arm-8.3-2019.03-x86_64-arm-eabi/bin:${PATH}"
 
 ARG WORKSPACE_ROOT
 VOLUME ${WORKSPACE_ROOT}
---
+++ cmake/Toolchain-ZYBO-Baremetal.cmake
@@ -0,0 +1,32 @@
+# cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/Toolchain-ZYBO-Baremetal.cmake ..
+
+# The Generic system name is used for embedded targets (targets without OS) in
+# CMake
+SET(CMAKE_SYSTEM_NAME          Generic)
+SET(CMAKE_SYSTEM_PROCESSOR     ARM)
+
+# Specify the cross compiler
+SET(CMAKE_C_COMPILER   arm-eabi-gcc)
+SET(CMAKE_CXX_COMPILER arm-eabi-g++)
+
+# Search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+
+# Set the CMake C and C++ flags (which should also be used by the assembler!)
+SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mfpu=neon" )
+SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mfloat-abi=hard" )
+SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -mcpu=cortex-a9" )
+
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mfpu=neon" )
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mfloat-abi=hard" )
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -mcpu=cortex-a9" )
+
+SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "" FORCE)
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}" CACHE STRING "" FORCE)
+# SET(CMAKE_ASM_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "" FORCE)
+
+SET(CMAKE_EXE_LINKER_FLAGS "--specs=nosys.specs"
+CACHE STRING "" FORCE)
