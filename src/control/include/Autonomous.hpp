3f0b5cbf667b3064caa0090de9cef4d36e3a0a50
btamm12 <bastiaan.tamm@student.kuleuven.be>
2019-04-24 21:46:36 +0200

    ready to compile?

--- src/control/include/Autonomous.hpp
+++ src/control/include/Autonomous.hpp
@@ -1,12 +1,20 @@
 // Original: BareMetal/src/control/timers.h (updates autonomous FSM + returns AutonomousOutput)
-#include <altitude.hpp>
-#include <position.hpp>
+#include <Altitude.hpp>
+#include <Position.hpp>
 
+/**
+ * Output of the autonomous control system, which consists of a reference
+ * position, reference height, whether the altitude controller should be
+ * bypassed and which common thrust should be used if it is bypassed.
+ */
 struct AutonomousOutput {
-    PositionReference location;
-    AltitudeReference height;
+    PositionReference referencePosition;
+    AltitudeReference referenceHeight;
     bool bypassAltitudeController;
     real_t commonThrust;
 };
 
+/**
+ * 
+ */
 AutonomousOutput updateAutonomousFSM();
