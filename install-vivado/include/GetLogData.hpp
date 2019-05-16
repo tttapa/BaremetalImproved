f9c92601f4bf37cf717c78e516e50fc375485f85
Pieter Pas <pieter.pas@student.kuleuven.be>
2019-05-17 00:15:04 +0200

    libs

--- install-vivado/include/GetLogData.hpp
+++ install-vivado/include/GetLogData.hpp
@@ -1,11 +1,1 @@
-#pragma once
-
-#include <LogEntry.hpp>
-#
-
-/**
- * @brief   Get a log entry with some of the global values already set.
- * 
- * @return  LogEntry 
- */
-LogEntry getLogData();
+../../../shared-linux/getlogdata/include/GetLogData.hpp
