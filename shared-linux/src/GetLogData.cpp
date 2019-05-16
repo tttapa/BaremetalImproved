a771237aad19d5ceed42fcc60e29d84f200aaa7e
btamm12 <btamm12@gmail.com>
2019-05-16 22:27:55 +0200

    updated logger

--- shared-linux/src/GetLogData.cpp
+++ shared-linux/src/GetLogData.cpp
@@ -1,24 +1,18 @@
 #include <GetLogData.hpp>
 
-#include <ControllerInstances.hpp>
-#include <Matrix.hpp>
-#include <MiscInstances.hpp>
-#include <RCValues.hpp>
-#include <Time.hpp>
-
 LogEntry getLogData() {
     LogEntry logentry;
-    logentry.altitudeControlSignal  = altitudeController.getControlSignal();
+    logentry.altitudeControlSignal = altitudeController.getControlSignal();
     logentry.altitudeIntegralWindup = altitudeController.getIntegralWindup();
-    logentry.altitudeMeasurement    = altitudeController.getMeasurement();
-    logentry.altitudeReference      = altitudeController.getReference();
-    logentry.altitudeStateEstimate  = altitudeController.getStateEstimate();
-    logentry.attitudeControlSignal  = attitudeController.getControlSignal();
+    logentry.altitudeMeasurement = altitudeController.getMeasurement();
+    logentry.altitudeReference = altitudeController.getReference();
+    logentry.altitudeStateEstimate = altitudeController.getStateEstimate();
+    logentry.attitudeControlSignal = attitudeController.getControlSignal();
     logentry.attitudeIntegralWindup = attitudeController.getIntegralWindup();
-    logentry.attitudeMeasurement    = attitudeController.getMeasurement();
-    logentry.attitudeReference      = attitudeController.getReference();
-    logentry.attitudeStateEstimate  = attitudeController.getStateEstimate();
-    logentry.positionControlSignal  = positionController.getControlSignal();
+    logentry.attitudeMeasurement = attitudeController.getMeasurement();
+    logentry.attitudeReference = attitudeController.getReference();
+    logentry.attitudeStateEstimate = attitudeController.getStateEstimate();
+    logentry.positionControlSignal = positionController.getControlSignal();
     logentry.positionIntegralWindup = positionController.getIntegralWindup();
     logentry.referenceLocation = positionController.getMeasurement();
     logentry.positionReference = positionController.getReference();
@@ -28,8 +22,9 @@ LogEntry getLogData() {
     logentry.rcInput = getRCInput();
     logentry.buzzerInstruction = buzzerManager.getCurrentInstruction();
 
-    logentry.millis    = getMillis();
+
+    logentry.millis = getMillis();
     logentry.tickCount = getTickCount();
-    logentry.time      = getTime();
+    logentry.time = getTime();
     return logentry;
 }
